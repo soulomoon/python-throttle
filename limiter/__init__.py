30d125a2c752c04693c2e3b0559492c0efe92f7f
soulomoon <fwy996602672@gmail.com>
2018-03-11 14:12:36 +0800

    Update limiter.py to rate_limiter.py

--- limiter/__init__.py
+++ limiter/__init__.py
@@ -1,2 +1,12 @@
+from redis import StrictRedis
+
+from .counter import SlidingRedisCounter, FixedWindowRedisCounter
+from .rate_limiter import RateLimiter
 
 
+def make_fixed_window_limiter(threshold, interval, redis_config) -> RateLimiter:
+    return RateLimiter(threshold, interval, FixedWindowRedisCounter(StrictRedis(**redis_config)))
+
+
+def make_sliding_limiter(threshold, interval, redis_config) -> RateLimiter:
+    return RateLimiter(threshold, interval, SlidingRedisCounter(StrictRedis(**redis_config)))
