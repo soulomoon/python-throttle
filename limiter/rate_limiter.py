f2b28d8534fae452dea0b94598ffd2f64ef6bb95
soulomoon <fwy996602672@gmail.com>
2018-03-11 15:56:21 +0800

    Update fix limiter to ceil input

--- limiter/rate_limiter.py
+++ limiter/rate_limiter.py
@@ -1,19 +1,29 @@
+import math
+
 from limiter.counter import AbstractionCounter
 
 
 class RateLimiter:
-    """rate limiter"""
+    """rate limiter, please give it a counter with add_key method..."""
 
     def __init__(self, threshold, interval, counter: AbstractionCounter):
         """
-        :param threshold: threshold
-        :param interval: window size resolution second
+        :param threshold: int, or we try to ceil
+        :param interval: int, or we try to ceil
         """
-        self.threshold = threshold
-        self.interval = interval
-        self.prefix = "rate-limiter:{}"
-        self.counter = counter
+        assert threshold > 0
+        assert interval > 0
+        self._threshold = math.ceil(threshold)
+        self._interval = math.ceil(interval)
+        self._prefix = "rate-limiter:{}"
+        self._counter = counter
 
     def exceeded(self, iid):
-        current = self.counter.add_key(self.prefix.format(iid), self.interval)
-        return current > self.threshold
+        current = self._counter.add_key(self._prefix.format(iid), self._interval)
+        return current >= self._threshold
+
+    def current(self, iid):
+        return self._counter.current(self._prefix.format(iid))
+
+    def reset(self, iid):
+        self._counter.reset(self._prefix.format(iid))
