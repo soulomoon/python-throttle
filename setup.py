b7350e2171914b62c10d2cacd3f16ef80c1fa413
soulomoon <fwy996602672@gmail.com>
2019-06-08 20:49:58 +0800

    remove support for python 3.4 due to pipenv

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
